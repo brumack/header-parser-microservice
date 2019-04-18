ef3ee46051438a49ec417c00025ef5e6470d25bc
Glitch (weak-node) <none>
2019-04-18 00:13:25 +0000

    initial commit

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for'].split(',')[0]
+  const language = req.headers['accept-language']
+  const software = req.headers['user-agent']
+  res.json({ipaddress, language, software})
+})
 
 
 // listen for requests :)
